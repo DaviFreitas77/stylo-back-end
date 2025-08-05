5f6d77107b03e56f21f3a07cd17541f9cb31de3c
Davi Freitas <davifreitaz999@gmail.com>
2025-08-05 00:05:20 -0300

    del procfile

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
 
-Route::get('/run-migrations', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return '✅ Migrations executadas com sucesso!';
-});
 
 Route::get('/', function () {
     return view('welcome');
